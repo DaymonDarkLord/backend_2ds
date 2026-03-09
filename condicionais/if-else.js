be9de0ddf5928315e469a17814ba2b23668d40c7
DaymonDarkLord <daymon.barros@escola.pr.gov.br>
2026-03-09 13:26:17 +0000

    Introdução  ao arrays

---
+++ condicionais/if-else.js
@@ -0,0 +1,16 @@
+ // if, else if, else, ternário
+ // <, >, <=, |=
+ // ||, &&
+
+ let idade = 18
+
+if(idade > 25{
+    console.log("Situação ímpar")
+    console.log("Traga um acompanhante")
+} else if (idade >= 25 && idade <= 25)
+    console.log("Entrada Liberada")
+    console.log("Festa sugerida suruba")
+} else {
+    console.log("Cai fora irmão")
+    console.log("Festa recomendada: aniversáeerio do McQueen")
+}
---
+++ variaveis/var-pessoa.js
@@ -0,0 +1,18 @@
+// , var, let, const
+// tipos de dados
+
+let nome = "Petter"       // string
+let sobrenome = "parker"    // string
+const cpf = "888887788-98"  // string
+const rg = "132131 ssp"     // string
+let idade = 22              //number
+let solteiro = true         //boolean
+let nota = 5 + 3        //number
+
+console.log(nome)
+console.log(sobrenome)
+console.log(cpf)
+console.log(rg)
+console.log(idade)
+console.log(solteiro)
+console.log(nota)
